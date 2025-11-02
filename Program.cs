243787ae28e1ddd9b09d6b1e764711d4aaa6f544
SK ASHRAFUL ALI APU <23-54917-3@student.aiub.edu>
2025-11-03 01:56:09 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Lab_task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter 'n' your desired number: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"Even numbers from 1 to {n}:");
+            
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
